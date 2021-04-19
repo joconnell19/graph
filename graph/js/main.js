1593118580faa387e8c0d5fa131933c4df531f43
Jamie <joconnell19@georgefox.edu>
2021-04-18 23:08:02 -0700

    improved

--- graph/js/main.js
+++ graph/js/main.js
@@ -29,14 +29,17 @@ function changeGraph() {
   let m = document.getElementById("m").value;
   let b = document.getElementById("b").value;
 
+  let xMin = document.getElementById("xMin").value;
+  let xMax = document.getElementById("xMax").value;
+
   var line = c.getContext("2d");
   line.clearRect(0, 0, line.width, line.height);
   line.beginPath();
   line.strokeStyle = 'mediumorchid';
   line.lineWidth = 4;
-  line.moveTo(400, 200 + (-400 * b));
-  line.lineTo(800, 200 + (-400 * b) + (m * -400));
-  line.lineTo(0, 200 + (-400 * b) + (m * 400));
+  line.moveTo(400, 200 + (-25 * b));
+  line.lineTo(400 + (400/15) * xMax, 200 + (-25 * b) + (m * ((-400/15) * xMax)));
+  line.lineTo((400/15) * (15 - -xMin), 200 + (-25 * b) + (m * (-400/15) * (xMin)));
   line.stroke();
   line.closePath();
 }
